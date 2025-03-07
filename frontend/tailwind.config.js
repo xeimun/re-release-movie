45b958d6c289a36d262288e37d1976d6f45573f2
xeimun <hinote444@naver.com>
2025-03-07 16:58:19 +0900

    feat: Tailwind CSS 설치

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{js,jsx,ts,tsx}"], // src 폴더 내 모든 JSX 파일에서 Tailwind 사용 가능
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
+
+
