e71eddd7edc2f1797bfc79cc92b5d1f10795ae18
xeimun <hinote444@naver.com>
2025-03-10 02:43:36 +0900

    feat: kofic Open API 연동

--- backend/src/main/java/com/rerelease/movie/rereleasemovie/config/CorsConfig.java
+++ backend/src/main/java/com/rerelease/movie/rereleasemovie/config/CorsConfig.java
@@ -5,10 +5,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-/**
- * CORS 설정 클래스
- * - React(프론트엔드, localhost:3000)에서 Spring Boot API(백엔드, localhost:8080)에 요청을 허용하도록 설정
- */
 @Configuration
 public class CorsConfig {
     @Bean
@@ -17,9 +13,9 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**") // API 경로 설정
-                        .allowedOrigins("http://localhost:3000") // React 도메인 허용
+                        .allowedOrigins("http://localhost:3000") // 실제 프론트엔드 URL 설정
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메서드
-                        .allowCredentials(true);
+                        .allowCredentials(true); // 인증 정보 포함 (JWT 사용 시 true로 설정)
             }
         };
     }
