a426408677406e19f53841ca1ec9eb74bcf60acb
xeimun <hinote444@naver.com>
2025-03-24 23:14:20 +0900

    refactor: CORS 설정 리팩토링 및 주석 추가

--- backend/src/main/java/com/rerelease/movie/rereleasemovie/config/SecurityConfig.java
+++ backend/src/main/java/com/rerelease/movie/rereleasemovie/config/SecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfigurationSource;
 
 @Configuration
 @RequiredArgsConstructor
@@ -21,13 +22,13 @@ public class SecurityConfig {
 
     private final JwtUtil jwtUtil;
     private final UserDetailsService userDetailsService;
+    private final CorsConfigurationSource corsConfigurationSource;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable()) // CSRF 보안 비활성화 (API 개발 시 필요)
-                .cors()
-                .and()
+                .cors(cors -> cors.configurationSource(corsConfigurationSource)) // Spring Security 필터 체인에 CORS 설정 적용
                 .sessionManagement(session -> session.sessionCreationPolicy(
                         SessionCreationPolicy.STATELESS)) // 세션 사용 안 함(JWT 인증 방식 사용)
                 .authorizeHttpRequests(auth -> auth
